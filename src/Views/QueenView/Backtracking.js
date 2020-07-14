732144b1e1427112ec099d419904edfe9ff33e8c
ChadiToto <chadi.toto@insa-cvl.fr>
2020-07-14 23:32:59 +0200

    Animations V2

--- src/Views/QueenView/Backtracking.js
+++ src/Views/QueenView/Backtracking.js
@@ -1,21 +1,26 @@
 const N = 4;
-let ld = new Array(30);
-let rd = new Array(30);
-let cl = new Array(30);
+
+function isSafe(board, row, col) {
+  for (let i = 0; i < col; i++) if (board[row][i] === 1) return false;
+  for (let i = row, j = col; i >= 0 && j >= 0; i--, j--)
+    if (board[i][j] === 1) return false;
+  for (let i = row, j = col; i < N && j >= 0; i++, j--)
+    if (board[i][j] === 1) return false;
+
+  return true;
+}
 
 function SolveNQUtil(board, col, animations) {
   /*Best case : All queens are placed*/
   if (col >= N) return true;
 
   for (let i = 0; i < N; i++) {
-    if (ld[i - col + N - 1] !== 1 && rd[i + col] !== 1 && cl[i] !== 1) {
+    if (isSafe(board, i, col)) {
       animations.push([i, col]);
       board[i][col] = 1;
-      ld[i - col + N - 1] = rd[i + col] = cl[i] = 1;
       if (SolveNQUtil(board, col + 1, animations)) return true;
       board[i][col] = 0; // Backtrack
       animations.push([i, col]);
-      ld[i - col + N - 1] = rd[i + col] = cl[i] = 0;
     }
   }
   return false;
--- src/Views/QueenView/QueenVizualizer.js
+++ src/Views/QueenView/QueenVizualizer.js
@@ -27,18 +27,36 @@ const useStyles = makeStyles({
 
 const QueenVizualizer = () => {
   const classes = useStyles();
+  const [board, setBoard] = useState([]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    resetBoard();
+  }, []);
 
   /**
    * This function resets the chessboard
    */
-  const resetBoard = () => {};
+  const resetBoard = () => {
+    const board = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ];
+
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        let tile = document.getElementById(i + "" + j).style;
+        tile.visibility = "hidden";
+      }
+    }
+    setBoard(board);
+  };
 
   /**
    * This function displays a board on the visualizer
    */
-  const setBoard = () => {
+  const displayBoard = () => {
     return (
       <Grid container direction="column">
         {[...Array(4)].map((x, i) => {
@@ -72,27 +90,15 @@ const QueenVizualizer = () => {
   };
 
   /**
-   * Reverts back Board to initial state
-   */
-  //const undo = () => {};
-
-  /**
    * Displays animation on the Vizualizer
    */
   const setAnimations = () => {
-    let board = [
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-    ];
     let animations = SolveNQ(board);
     for (let i = 0; i < animations.length; i++) {
       setTimeout(() => {
         let tile = document.getElementById(
           animations[i][0] + "" + animations[i][1]
         );
-        console.log(tile.style.visibility);
         if (tile.style.visibility === "hidden") tile.style.visibility = "";
         else tile.style.visibility = "hidden";
       }, i * 500);
@@ -124,12 +130,16 @@ const QueenVizualizer = () => {
       <Grid container style={{ marginBottom: 40, marginTop: 20 }}>
         <Grid item xs={2} sm={4} />
         <Grid item xs={10} sm={4} style={{ marginRight: 60 }}>
-          <Paper>{setBoard()}</Paper>
+          <Paper>{displayBoard()}</Paper>
         </Grid>
       </Grid>
 
       {/* Player Part */}
-      <Player options={algorithms}></Player>
+      <Player
+        options={algorithms}
+        reset={resetBoard}
+        undo={resetBoard}
+      ></Player>
     </Grid>
   );
 };
